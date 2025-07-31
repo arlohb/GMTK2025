f855b4b53684003e2a9b7e1570ae818b62f67493
arlohb <arlo@urwick.co.uk>
2025-07-31 11:32:00 +0100

    Added basic beat

--- Assets/Scripts/PianoRoll.cs
+++ Assets/Scripts/PianoRoll.cs
@@ -6,6 +6,13 @@ public class PianoRoll : MonoBehaviour
     public int beatCount = 8;
     public float padding = 1;
 
+    public int bpm = 100;
+    private float beatLength;
+    private float barLength;
+    private int currentBeat = 0;
+    private int currentBar = 1;
+    private float startTime;
+
     public GameObject square;
 
     private bool[,] notes;
@@ -34,12 +41,45 @@ public class PianoRoll : MonoBehaviour
                 note.Setup(y, x, SetNote);
             }
         }
+
+        startTime = Time.time;
+        beatLength = 60f / bpm;
+        barLength = beatCount * beatLength;
     }
 
-    // Update is called once per frame
+    void PlayBeat()
+    {
+        Debug.Log("Playing beat " + currentBar + "-" + currentBeat);
+        GetComponent<AudioSource>().Play();
+    }
+
+    void UpdateBeat()
+    {
+        float currentTime = Time.time - startTime;
+        int bar = 1 + (int)(currentTime / barLength);
+
+        if (bar == currentBar)
+        {
+            float timeInBar = currentTime % barLength;
+            int beat = 1 + (int)(timeInBar / beatLength);
+
+            if (beat != currentBeat)
+            {
+                currentBeat = beat;
+                PlayBeat();
+            }
+        }
+        else
+        {
+            currentBeat = 1;
+            currentBar = bar;
+            PlayBeat();
+        }
+    }
+
     void Update()
     {
-
+        UpdateBeat();
     }
 
     void SetNote(int instrument, int beat, bool value)
