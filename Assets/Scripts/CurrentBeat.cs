f843dcbb7b3dbd2f1f05f1817222c09666581733
arlohb <arlo@urwick.co.uk>
2025-07-31 14:23:43 +0100

    Added current beat display

---
+++ Assets/Scripts/CurrentBeat.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CurrentBeat : MonoBehaviour
+{
+    private float xOffset;
+
+    void Start()
+    {
+        BeatManager.Get().RegisterListener(NewBeat);
+
+        PianoRoll pianoRoll = gameObject.GetComponentInParent<PianoRoll>();
+        float squareWidth = pianoRoll.square.GetComponent<Renderer>().bounds.size.x;
+        float squareHeight = pianoRoll.square.GetComponent<Renderer>().bounds.size.y;
+
+        float padding = (transform.localScale.x - squareWidth) / 2f;
+
+        int count = pianoRoll.instruments.Length;
+        float height = count * (squareHeight + pianoRoll.padding) - pianoRoll.padding + 2 * padding;
+        transform.localScale = new(transform.localScale.x, height, transform.localScale.z);
+        transform.localPosition = new(
+            transform.localPosition.x,
+            (count - 1) * (squareHeight + pianoRoll.padding) / 2f,
+            transform.localPosition.z
+        );
+
+        xOffset = squareWidth + pianoRoll.padding;
+    }
+
+    void NewBeat(int _currentBar, int currentBeat)
+    {
+        transform.localPosition = new(
+            xOffset * (currentBeat - 1),
+            transform.localPosition.y,
+            transform.localPosition.z
+        );
+    }
+}
