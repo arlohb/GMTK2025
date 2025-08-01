deb2b6564cc2b17aa1dd1faa93467af8f427186e
arlohb <arlo@urwick.co.uk>
2025-08-01 23:54:38 +0100

    Fixed background speed at different bpm

--- Assets/Scripts/Metronome.cs
+++ Assets/Scripts/Metronome.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Metronome : MonoBehaviour
 {
+    public AudioMixerGroup pitchBendMixer;
     public AudioSource firstBeat;
     public AudioSource beat;
     public AudioSource background;
@@ -9,6 +11,17 @@ public class Metronome : MonoBehaviour
     void Start()
     {
         BeatManager.Get().RegisterListener(NewBeat);
+
+        float bpm = BeatManager.Get().bpm;
+        if (bpm != 100f)
+        {
+            float scale = bpm / 100f;
+
+            // Bit of a hack to speed up audio without changing the pitch
+            // https://discussions.unity.com/t/how-i-can-change-the-speed-of-a-song-or-sound/6623/6
+            background.pitch = scale;
+            pitchBendMixer.audioMixer.SetFloat("PitchBend", 1f / scale);
+        }
     }
 
     void NewBeat(int _currentBar, int currentBeat)
